8c757a4ae2c91733eaba989e3b841da62d356da3
Antares <elgranpalicron@gmail.com>
2026-07-17 22:35:24 -0500

    class 21

---
+++ Source/MonsterWorld/Public/Character/MW_RedCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "MW_RedCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MONSTERWORLD_API AMW_RedCharacter : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
