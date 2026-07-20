ea4158bcd6b60f437f162251a8f212626dbbba1c
Antares <elgranpalicron@gmail.com>
2026-07-20 12:32:31 -0500

    class 24

--- Source/MonsterWorld/Private/Character/MW_RedCharacter.cpp
+++ Source/MonsterWorld/Private/Character/MW_RedCharacter.cpp
@@ -4,6 +4,7 @@
 #include "Character/MW_RedCharacter.h"
 
 #include "EnhancedInputComponent.h"
+#include "PaperFlipbookComponent.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
@@ -27,12 +28,43 @@ void AMW_RedCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
 	{
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AMW_RedCharacter::Move);
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Completed, this, &AMW_RedCharacter::EndMove);
 	}
 }
 
 void AMW_RedCharacter::Move(const FInputActionValue& InputValue)
 {
-	const FVector2D MoveAxis = InputValue.Get<FVector2D>();
+	MoveAxis = InputValue.Get<FVector2D>();
 	const FVector MoveDirection = FVector(MoveAxis.X, MoveAxis.Y, 0);
 	AddMovementInput(MoveDirection, 1);
+	
+	SetFlipBookAnimation();
+
 }
+
+
+void AMW_RedCharacter::SetFlipBookAnimation(const bool bTransitionToIdle) const
+{
+	if (MoveAxis.X > 0)
+	{
+		GetSprite()->SetFlipbook(bTransitionToIdle ? Right_Idle : Right_Walking);
+	}
+	else if (MoveAxis.X < 0)
+	{
+		GetSprite()->SetFlipbook(bTransitionToIdle ? Left_Idle : Left_Walking);
+	}
+	else if (MoveAxis.Y > 0)
+	{
+		GetSprite()->SetFlipbook(bTransitionToIdle ? Down_Idle : Down_Walking);
+	}
+	else if (MoveAxis.Y < 0)
+	{
+		GetSprite()->SetFlipbook(bTransitionToIdle ? Up_Idle : Up_Walking);
+	}
+}
+
+void AMW_RedCharacter::EndMove(const FInputActionValue& InputValue)
+{
+	SetFlipBookAnimation(true);
+}
+
--- Source/MonsterWorld/Public/Character/MW_RedCharacter.h
+++ Source/MonsterWorld/Public/Character/MW_RedCharacter.h
@@ -7,6 +7,7 @@
 #include "PaperCharacter.h"
 #include "MW_RedCharacter.generated.h"
 
+class UPaperFlipbook;
 class UInputAction;
 class UCameraComponent;
 class USpringArmComponent;
@@ -24,6 +25,8 @@ public:
 	
 	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 	
+	UFUNCTION(BlueprintCallable)
+	FVector2D GetMoveAxis() const { return MoveAxis; };
 protected:
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
@@ -32,11 +35,44 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
 	TObjectPtr<UCameraComponent> CameraComponent;
 	
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category= "Animation")
+	TObjectPtr<UPaperFlipbook> Down_Idle;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category= "Animation")
+	TObjectPtr<UPaperFlipbook> Down_Walking;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category= "Animation")
+	TObjectPtr<UPaperFlipbook> Up_Idle;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category= "Animation")
+	TObjectPtr<UPaperFlipbook> Up_Walking;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category= "Animation")
+	TObjectPtr<UPaperFlipbook> Right_Idle;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category= "Animation")
+	TObjectPtr<UPaperFlipbook> Right_Walking;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category= "Animation")
+	TObjectPtr<UPaperFlipbook> Left_Idle;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category= "Animation")
+	TObjectPtr<UPaperFlipbook> Left_Walking;
+	
 	UPROPERTY(EditDefaultsOnly,Category= "Inputs")
 	TObjectPtr<UInputAction> MoveAction;
 	
+	UPROPERTY()
+	FVector2D MoveAxis;
+	
 	UFUNCTION()
 	void Move(const FInputActionValue& InputValue);
+	
+	UFUNCTION()
+	void EndMove(const FInputActionValue& InputValue);
+	
+	UFUNCTION()
+	void SetFlipBookAnimation(const bool bTransitionToIdle = false) const;
 	
 	
 	
